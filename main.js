9d3c3496bc8e45e1f4a534776f9a484df9d17eda
nsp5488 <nsp5488@g.rit.edu>
2024-05-15 01:42:42 -0400

    Finished

--- main.js
+++ main.js
@@ -8,7 +8,27 @@ async function main() {
     }
     console.log(`Beginning crawl from base URL: ${argv[2]}`)
     let pages = await crawlPage(argv[2]);
-    console.log(pages)
+    printReport(pages, argv[2])
 }
 
+function sortPages(pages) {
+    let pageArray = []
+    for(let page in pages) {
+        pageArray.push({'count':pages[page], 'page': page})
+    }
+
+    pageArray.sort((a, b) => {
+        a.count > b.count
+    })
+    return pageArray;
+}
+
+function printReport(pages, url) {
+    console.log(`Report starting for ${url}!`);
+    const sortedPages = sortPages(pages);
+    for(const page of sortedPages) {
+        console.log(`Found ${page.count} internal link${page.count == 1 ? '' : 's'} to ${page.page}`)
+    }
+
+}
 main()
